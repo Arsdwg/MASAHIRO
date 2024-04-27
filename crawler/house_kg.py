7f7afd47c9308624a90dddfd8fdb5dff0bd95f00
ars <163355758+Arsdwg@users.noreply.github.com>
2024-04-27 10:44:50 +0600

    асинхрон

--- crawler/house_kg.py
+++ crawler/house_kg.py
@@ -1,3 +1,4 @@
+import asyncio
 import httpx
 from parsel import Selector
 from aiogram import Router, F, types
@@ -10,31 +11,45 @@ class DomCrawler:
     MAIN_URL = 'https://www.house.kg/snyat'
     BASE_URL = 'https://www.house.kg'
 
-    def get_page(self):
-        response = httpx.get(self.MAIN_URL)
-        self.page = response.text
+    async def get_page(self, url: str, client: httpx.AsyncClient):
+        response = await client.get(url)
+        return response.text
 
-    def get_title(self):
-        html = Selector(self.page)
+    def get_title(self, page: str):
+        html = Selector(page)
         title = html.css('title::text').get()
         return title
 
-    def get_houses_link(self):
-        html = Selector(self.page)
+    def get_houses_links(self, page: str):
+        html = Selector(page)
         links = html.css('.title a::attr(href)').getall()
         full_links = list(map(lambda x: self.BASE_URL + x, links))
-        return full_links[:10]
+        return full_links
+
+    async def get_houses(self):
+        list = []
+        async with httpx.AsyncClient() as client:
+            for i in range(1,11):
+                url = f'{self.MAIN_URL}?page={i}'
+                listik = asyncio.create_task(self.get_page(url, client))
+                list.append(listik)
+            results = await asyncio.gather(*list)
+            all_links = []
+            for result in results:
+                links = self.get_houses_links(result)
+                all_links.extend(links)
+        for i, link in enumerate(all_links, start=1):
+            print(f"{i}. {link}")
+        return all_links[:9]
 
 
 @check_router.callback_query(F.data == 'check')
 async def get_checked(cb: types.CallbackQuery):
+    await cb.answer()
     check = DomCrawler()
-    check.get_page()
-    list = check.get_houses_link()
-    for item in list:
-        await cb.message.answer(str(item))
-
-    # await message.answer()
+    test = await check.get_houses()
+    for link in test:
+        await cb.message.answer(link)
 
 
 
